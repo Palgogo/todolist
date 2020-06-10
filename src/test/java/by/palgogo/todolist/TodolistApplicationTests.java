c3392b354c63b56874beef3d7569726b977014dd
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-06-10 14:58:50 +0300

    initial commit

---
+++ src/test/java/by/palgogo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package by.palgogo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
