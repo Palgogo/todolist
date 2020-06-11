1d68ce2e01bfe6e47fadd0dcc318e3f324297ca8
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-06-11 11:53:48 +0300

    Add simple CRUD services for Task and Category.

--- src/main/java/by/palgogo/todolist/repository/TaskRepository.java
+++ src/main/java/by/palgogo/todolist/repository/TaskRepository.java
@@ -4,6 +4,11 @@ import by.palgogo.todolist.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByCategory_Id(Long categoryId);
+    List<Task> findAllByDoneStatusFalse();
 }
