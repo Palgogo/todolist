aff0e69992e88ded0e6e72c7caa879800b0384d4
Pavel Moysyuk-Dranko <moysyukdrankopasha@gmail.com>
2020-06-11 13:15:25 +0300

    Add basic task controller methods.

---
+++ src/main/java/by/palgogo/todolist/web/rest/TaskResource.java
@@ -0,0 +1,62 @@
+package by.palgogo.todolist.web.rest;
+
+import by.palgogo.todolist.domain.Task;
+import by.palgogo.todolist.repository.TaskRepository;
+import by.palgogo.todolist.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+@Transactional
+public class TaskResource {
+
+    @Autowired
+    private TaskService taskService;
+
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("tasks/{id}")
+    public ResponseEntity<?> getTask(@PathVariable Long id) {
+        Optional<Task> task = taskService.getTaskById(id);
+        return (ResponseEntity<?>) task.map((response) -> ((ResponseEntity.BodyBuilder) ResponseEntity.ok().body(response)))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    }
+
+    @GetMapping("tasks/category/{categoryId}")
+    public List<Task> getCategoryTasks(@PathVariable Long categoryId){
+        return taskService.getAllTasksInCategory(categoryId);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/tasks")
+    public ResponseEntity<Task> createTask(@Valid @RequestBody Task task) throws URISyntaxException {
+        Task result = taskService.createTask(task);
+
+        return ResponseEntity.created(new URI("api/tasks/" + result.getId()))
+            .body(result);
+    }
+
+}
