fe0ecb8b1e4c6a0fef1b7ca013433216e6cff1cd
Pavel Moisiuk-Dranko <ipalgogo@gmail.com>
2020-06-13 21:06:02 +0300

    add task and category dto

---
+++ src/main/java/by/palgogo/todolist/service/dto/CategoryDTO.java
@@ -0,0 +1,6 @@
+package by.palgogo.todolist.service.dto;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/by/palgogo/todolist/service/dto/TaskDTO.java
@@ -0,0 +1,9 @@
+package by.palgogo.todolist.service.dto;
+
+public class TaskDTO {
+
+    private Long id;
+    private String description;
+    private Boolean doneStatus;
+    private Long categoryId;
+}
