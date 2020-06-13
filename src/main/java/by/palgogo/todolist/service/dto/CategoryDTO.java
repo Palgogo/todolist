aab487fc31ad10db1a4cc2fd1a57f6e224a10cd7
Pavel Moisiuk-Dranko <ipalgogo@gmail.com>
2020-06-13 21:06:56 +0300

    add task and category getter/setter

--- src/main/java/by/palgogo/todolist/service/dto/CategoryDTO.java
+++ src/main/java/by/palgogo/todolist/service/dto/CategoryDTO.java
@@ -1,5 +1,10 @@
 package by.palgogo.todolist.service.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CategoryDTO {
     private Long id;
     private String name;
--- src/main/java/by/palgogo/todolist/service/dto/TaskDTO.java
+++ src/main/java/by/palgogo/todolist/service/dto/TaskDTO.java
@@ -1,5 +1,10 @@
 package by.palgogo.todolist.service.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TaskDTO {
 
     private Long id;
